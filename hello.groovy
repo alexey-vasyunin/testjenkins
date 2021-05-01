635edd3d31dbd3322dd5fff82ad574223f943125
Alexey <a.vasyunin@gmail.com>
2021-05-02 00:54:27 +0400

    Create hello.groovy

---
+++ hello.groovy
@@ -0,0 +1,8 @@
+#!/usr/bin/env groovy
+
+class Example {
+   static void main(String[] args) {
+      // Using a simple println statement to print output to the console
+      println('Hello World');
+   }
+}
